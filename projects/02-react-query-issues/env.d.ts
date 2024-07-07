83d43feb4b69f90a778365a4a826549d15a3e0ac
angelgamonal <angelgamonal.t@gmail.com>
2024-07-07 01:34:00 -0500

    add env config

---
+++ projects/02-react-query-issues/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly VITE_VITE_TOKEN_GITHUB: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- projects/02-react-query-issues/src/api/githubApi.ts
+++ projects/02-react-query-issues/src/api/githubApi.ts
@@ -4,5 +4,7 @@ const API = "https://api.github.com/repos/facebook/react";
 
 export const githubApi = axios.create({
   baseURL: API,
-  headers: {},
+  headers: {
+    Authorization: `Bearer ${String(import.meta.env.VITE_TOKEN_GITHUB)}`,
+  },
 });
