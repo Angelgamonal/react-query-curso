081bc755d342b9a0ff2ad68677620af09e80a298
angelgamonal <angelgamonal.t@gmail.com>
2024-07-10 16:34:46 -0500

    section 4

---
+++ projects/02-react-query-issues/src/issues/hooks/useIssue.tsx
@@ -0,0 +1,38 @@
+import { githubApi } from "../../api/githubApi";
+import { sleep } from "../../helpers/sleep";
+import { Issue } from "../interfaces";
+import { useQuery } from "@tanstack/react-query";
+
+interface Props {
+  issueNumber: number;
+}
+
+const getIssueInfo = async (issueNumber: number) => {
+  await sleep(2);
+  const { data } = await githubApi.get<Issue>(`/issues/${issueNumber}`);
+
+  return data;
+};
+
+const getIssueComments = async (issueNumber: number) => {
+  await sleep(2);
+  const { data } = await githubApi.get<Issue[]>(
+    `/issues/${issueNumber}/comments`
+  );
+
+  return data;
+};
+
+export const useIssue = (issueNumber: number) => {
+  const issueQuery = useQuery({
+    queryKey: ["issue", issueNumber],
+    queryFn: () => getIssueInfo(issueNumber),
+  });
+
+  const commentsQuery = useQuery({
+    queryKey: ["issue", issueNumber, "comments"],
+    queryFn: () => getIssueComments(issueNumber),
+  });
+
+  return { issueQuery, commentsQuery };
+};
--- projects/02-react-query-issues/src/issues/views/ListView.tsx
+++ projects/02-react-query-issues/src/issues/views/ListView.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { IssueList } from "../components/IssueList";
 import { LabelPicker } from "../components/LabelPicker";
+import { useIssues } from "../hooks";
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
 
 export const ListView = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+  const { issuesQuery } = useIssues();
 
   const onLabelChanged = (labelName: string) => {
     const selected = selectedLabels.includes(labelName)
@@ -18,7 +21,11 @@ export const ListView = () => {
   return (
     <div className="row mt-5">
       <div className="col-8">
-        <IssueList />
+        {issuesQuery.isLoading ? (
+          <LoadingIcon />
+        ) : (
+          <IssueList issues={issuesQuery.data ?? []} />
+        )}
       </div>
 
       <div className="col-4">
